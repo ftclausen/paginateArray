dceea8dd21b7f027fc60b5efd854432cb3314405
Friedrich Clausen <friedrich.clausen@blackboard.com>
2022-06-07 09:43:29 +1000

    Use closure for ease

--- paginate.groovy
+++ paginate.groovy
@@ -1,14 +1,19 @@
 #!/usr/bin/env groovy
 
-def sampleData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-def segmentSize = 2
 def index = 0
 
-while (index < sampleData.size()) {
-  if (index + segmentSize > sampleData.size()) {
-    println sampleData[index..sampleData.size() - 1]
-    break
+def withWhile(segmentSize, sampleData, Closure code) {
+  def index = 0
+  while (index < sampleData.size()) {
+    if (index + segmentSize > sampleData.size()) {
+      code(sampleData[index..sampleData.size() - 1])
+      break
+    }
+    code(sampleData[index..index+segmentSize - 1])
+    index += segmentSize
   }
-  println sampleData[index..index+segmentSize - 1]
-  index += segmentSize
 }
+
+def sampleData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+def segmentSize = 2
+withWhile(3, sampleData) { it -> println it }
